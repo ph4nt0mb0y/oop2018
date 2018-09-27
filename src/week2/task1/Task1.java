13f24d038adaf30c896f42e280ad793aac58bc5f
Nhok <43149414+ph4nt0mb0y@users.noreply.github.com>
2018-09-27 23:21:27 +0700

    Add files via upload

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,11 +4,18 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+		int i;
+		for(i=Math.abs(a);i>0;i--){
+			if(Math.abs(a)%i==0&&Math.abs(b)%i==0) break;
+		}
+		if(a>0&&b>0) return i;
+		else return i*-1;
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+		if (n==0) return 0;
+		if (n==1) return 1;
+        return fibonacci(n-1)+fibonacci(n-2);
     }
 }
