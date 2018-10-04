22220a39e4bea6309f6e98c7ade6800fb5644978
Võ Hồng Long <hdragon200399@gmail.com>
2018-10-04 21:38:47 +0700

    UPDATE

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -12,7 +12,7 @@ public class Week3 {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
 		int min=array[0];
 		for(int i=0;i<array.size();i++){
-			if(a[i]<min) min = a[i];
+			if(array[i]<min) min = array[i];
 		}
         return min;
     }
@@ -25,7 +25,7 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-		int BMI = weight/(height*height);
+		double BMI = weight/(height*height);
 		if(BMI<18.5) return "Thieu can";
         if(BMI>=18.5&&BMI<23) return "Binh thuong";
 		if(BMI>=23&&BMI<=24.99) return "Thua can";
